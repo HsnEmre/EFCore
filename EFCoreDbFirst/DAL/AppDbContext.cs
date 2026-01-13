e15802f3c30849762ae89720151a14d0e224ef83
Hasan Emre Buzkan <92093161+HsnEmre@users.noreply.github.com>
2026-01-13 06:31:39 +0300

    db first short

---
+++ EFCoreDbFirst/DAL/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreDbFirst.DAL
+{
+    public class AppDbContext:DbContext//miras
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-41V1OLM\\SQLEXPRESS;Initial Catalog=EFCoreDatabaseFirstDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");//connectionstring
+
+        }
+
+    }
+}
--- EFCoreDbFirst/Program.cs
+++ EFCoreDbFirst/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+
+using EFCoreDbFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+using (var _context = new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+
+    products.ForEach(x =>
+    {
+        Console.WriteLine($"{x.id} {x.Name}");
+
+    });
+    Console.ReadLine();
+
+}
